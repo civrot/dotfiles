1d5cfb0a7b081e8986406a069f166094b8d126c3
Victor Asteinza <victor.asteinza@pgn.com>
2010-04-05 12:19:12 -0700

    Tweeking the prompt layout some more.

--- workspace_select.sh
+++ workspace_select.sh
@@ -17,7 +17,7 @@ do
       export WID_WORKSPACE_NAME=$workspace
       export WID_WORKSPACE_DIR="$WID_WORKSPACES/$workspace"
       #export PS1="\e[1;35mWID - $WID_WORKSPACE_NAME\e[0;37m$(__git_ps1 " (%s)")\n[\u@\h \W]\$ "
-      export PS1="\e[1;35mWID - $WID_WORKSPACE_NAME$LIGHT_CYAN\$(scm_display)$DEFAULT_COLOR[\u@\h \W]\$ "
+      export PS1="\e[1;35mWID - $WID_WORKSPACE_NAME - $LIGHT_CYAN\$(scm_display)$DEFAULT_COLOR\n[\u@\h \W]\$ "
       cd $WID_WORKSPACE_DIR
       break
     ;;
