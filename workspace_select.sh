3dbce9bb1b31828b53f5b07d2300ebb455fd0dc2
Victor Asteinza <victor.asteinza@pgn.com>
2010-04-05 11:04:34 -0700

    Adding updated files.  The svn status is still broken for trunk.

--- workspace_select.sh
+++ workspace_select.sh
@@ -16,7 +16,8 @@ do
       echo "Switching to $WID_WORKSPACE_NAME"
       export WID_WORKSPACE_NAME=$workspace
       export WID_WORKSPACE_DIR="$WID_WORKSPACES/$workspace"
-      export PS1="\e[1;35mWID - $WID_WORKSPACE_NAME\n\e[0;37m[\u@\h \W]\$ "
+      #export PS1="\e[1;35mWID - $WID_WORKSPACE_NAME\e[0;37m$(__git_ps1 " (%s)")\n[\u@\h \W]\$ "
+      export PS1="\e[1;35mWID - $WID_WORKSPACE_NAME$LIGHT_CYAN\$(scm_display)$DEFAULT_COLOR[\u@\h \W]\$ "
       cd $WID_WORKSPACE_DIR
       break
     ;;
