fd471277188b606ecb106a1f975a86b898da67c9
Victor Asteinza <victor.asteinza@pgn.com>
2010-04-26 11:50:15 -0700

    Fixing the files that I deleted before commiting.

--- workspace_select.sh
+++ workspace_select.sh
@@ -1,7 +1,6 @@
 #/bin/bash
 
 ENV_DIR=~/.env
-WID_WORKSPACES=~/code/WID
 
 PS3="Choose workspace (0 to cancel) :"
 
@@ -13,11 +12,11 @@ do
       break
     ;;
     *)
-      echo "Switching to $WID_WORKSPACE_NAME"
       export WID_WORKSPACE_NAME=$workspace
+      echo "Switching to $WID_WORKSPACE_NAME"
       export WID_WORKSPACE_DIR="$WID_WORKSPACES/$workspace"
       #export PS1="\e[1;35mWID - $WID_WORKSPACE_NAME\e[0;37m$(__git_ps1 " (%s)")\n[\u@\h \W]\$ "
-      export PS1="\e[1;35mWID - $WID_WORKSPACE_NAME - $LIGHT_CYAN\$(scm_display)$DEFAULT_COLOR\n[\u@\h \W]\$ "
+      export PS1="\e[1;35m$WID_WORKSPACE_NAME  $LIGHT_CYAN\$(scm_display)$DEFAULT_COLOR\n[\u@\h \W]\$ "
       cd $WID_WORKSPACE_DIR
       break
     ;;
