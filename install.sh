fc77c6ea9835d42f8eb232cc7cba27a4c6e9ed05
Victor Asteinza <victor@theclymb.com>
2013-08-22 17:13:04 -0700

    adding install script

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# borrowed from https://github.com/thoughtbot/dotfiles/commit/fdbd7b52486c166230931b678f810afcae5f14a9#install.sh
+
+for name in *; do
+  target="$HOME/.$name"
+  if [ -e "$target" ]; then
+    if [ ! -L "$target" ]; then
+      echo "WARNING: $target exists but is not a symlink."
+    fi
+  else
+    if [ "$name" != 'install.sh' ] && [ "$name" != 'README.md' ]; then
+      echo "Creating $target"
+      ln -sf "$PWD/$name" "$target"
+    fi
+  fi
+done
+
+git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+vim -u ~/.vimrc.bundles +BundleInstall! +qa
